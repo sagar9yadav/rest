d3df49eee6d9f757b120a3cec3104fea8101a6fc
sagar.9yadav <sagar.9yadav@gmail.com>
2017-05-22 01:37:26 +0530

    Rest Project

---
+++ rest-test1/src/fi/op/banking/cards/HelloWorldService.java
@@ -0,0 +1,26 @@
+package fi.op.banking.cards;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path(value = "/hello")
+public class HelloWorldService {
+	static {
+		System.out.println("\nHelloWorldService::static::block::");
+	}
+
+	public HelloWorldService() {
+		System.out.println("\nHelloWorldService()::args()::");
+	}
+
+	@GET
+	@Path(value = "/showMessage/{param}")
+	public Response getMessage(@PathParam("param") String message) {
+		System.out.println("\nHelloWorldService::getMessage()::start::");
+		String output = "Jesrsey say" + message;
+		System.out.println("HelloWorldService::getMessage()::end::");
+		return Response.status(200).entity(output).build();
+	}
+}
