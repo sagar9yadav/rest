3ff076872d41b8599afe6e05a4adf4e23e663fab
yadav510 <yadav510@VDIT7P02676.kpnnl.local>
2018-05-16 19:28:26 +0530

    Added Java Docs

--- rest-test1/src/fi/op/banking/cards/HelloWorldService.java
+++ rest-test1/src/fi/op/banking/cards/HelloWorldService.java
@@ -5,6 +5,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+/**
+ * @author yadav510
+ *
+ */
 @Path(value = "/hello")
 public class HelloWorldService {
 	static {
